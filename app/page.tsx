04e0a27d79812bc2ffaa8a8008fafc86d89689cd
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-08 12:38:43 +0530

    JG: create main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { Ex } from "@/components/Ex";
+import { useAppSelector } from "@/redux/store";
+
 export default function Home() {
+  const task = useAppSelector((state) => state.task);
+  console.log(task);
   return (
-    <>
-      <main>
-        <h1>Hello jaggu</h1>
-      </main>
-    </>
+    <main>
+      <h1>Hello jaggu</h1>
+      <p>
+        <Ex />
+      </p>
+    </main>
   );
 }
