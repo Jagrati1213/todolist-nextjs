73edb5735b90e9cadcf09fcf95eaa0e1a12b6baf
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-09 16:51:16 +0530

    JG: deletes files

--- app/(pages)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./page.module.scss";
-
-function DashBoard() {
-  return <main className={style.main}>personal tasks</main>;
-}
-
-export default DashBoard;
--- app/(pages)/dashboard/weektasks/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function weektasks() {
-  return <div>weektasks</div>;
-}
-
-export default weektasks;
