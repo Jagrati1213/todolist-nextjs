7616f675997107c941e7121fe605596dacfffcf8
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-09 14:44:45 +0530

    JG: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { Ex } from "@/components/Ex";
-import { useAppSelector } from "@/redux/store";
+import React from "react";
 
-export default function Home() {
-  const task = useAppSelector((state) => state.task);
-  console.log(task);
-  return (
-    <main>
-      <h1>Hello jaggu</h1>
-      <p>
-        <Ex />
-      </p>
-    </main>
-  );
+function HomePage() {
+  return <div>HomePage</div>;
 }
+
+export default HomePage;
