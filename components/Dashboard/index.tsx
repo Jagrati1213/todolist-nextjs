157c1e8da0e17c910c0e5e6673624c9534e0cae6
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 19:19:27 +0530

    JG: add classname

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import { Col } from "antd";
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import SideMenu from "./SideMenu";
+import style from "./index.module.scss";
 
 export function Dashboard() {
   const [collapsed, setCollapsed] = useState(false);
@@ -13,7 +14,7 @@ export function Dashboard() {
 
   return (
     <>
-      <Col span={24}>
+      <Col span={24} className={style.fixed_header}>
         <Navbar toggleCollapsed={toggleCollapsed} collapsed={collapsed} />
       </Col>
       <SideMenu collapsed={collapsed} />
