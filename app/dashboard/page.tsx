599e9060e042708f29422a2476ba254f3f14c8bf
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 18:57:40 +0530

    JG: add condition in routing

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import style from "./page.module.scss";
-import { Col, Flex, Input, Row, Space } from "antd";
+import { Col, Flex, Input, Row } from "antd";
 import TaskList from "@/components/Dashboard/TaskList";
+import { useRouter } from "next/navigation";
 
 function dashboard() {
+  const router = useRouter();
+  const user = "ram";
+
+  useEffect(() => {
+    if (user !== null) {
+      return router.push("/dashboard");
+    } else {
+      return router.push("/");
+    }
+  }, [user]);
   return (
     <section className={style.dashboard}>
       <Flex className="container" align={"center"} vertical>
