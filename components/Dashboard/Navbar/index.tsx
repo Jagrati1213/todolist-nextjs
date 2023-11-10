8e60e2d39776013dbec1551468be8884a74c69c9
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 19:02:50 +0530

    JG: add dropdown in profile

--- components/Dashboard/Navbar/index.tsx
+++ components/Dashboard/Navbar/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { Space, Avatar } from "antd";
+import { Space, Avatar, Dropdown, Button } from "antd";
 import { MenuOutlined, CloseOutlined, UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
 import style from "./index.module.scss";
 
 interface MenuTypes {
   collapsed?: boolean;
   toggleCollapsed?: () => void;
 }
+
 function Navbar({ collapsed, toggleCollapsed }: MenuTypes) {
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: <span>Jagrati gupta</span>,
+    },
+    {
+      key: "2",
+      label: <Button>Sign Out</Button>,
+    },
+  ];
   return (
     <header className={style.navbar}>
       <Space>
@@ -17,7 +29,9 @@ function Navbar({ collapsed, toggleCollapsed }: MenuTypes) {
         <div className={style.logo}>TakeNote</div>
       </Space>
       <div className="profile">
-        <Avatar shape="circle" size={"large"} icon={<UserOutlined />} />
+        <Dropdown menu={{ items }} placement="bottomLeft">
+          <Avatar shape="circle" size={"large"} icon={<UserOutlined />} />
+        </Dropdown>
       </div>
     </header>
   );
