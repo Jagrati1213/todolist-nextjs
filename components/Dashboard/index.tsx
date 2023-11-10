b3764cd22a5bbbe59e4fb556ecaba69b3c6899cc
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 12:36:00 +0530

    JG: add new component

---
+++ components/Dashboard/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Col } from "antd";
+import React, { useState } from "react";
+import Navbar from "../Navbar";
+import SideMenu from "../SideMenu";
+
+export function Dashboard() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <>
+      <Col span={24}>
+        <Navbar toggleCollapsed={toggleCollapsed} collapsed={collapsed} />
+      </Col>
+      <SideMenu collapsed={collapsed} />
+    </>
+  );
+}
