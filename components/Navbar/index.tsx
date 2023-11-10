ad93bc4cf24fdfbe90bc266a66937ddb193452c1
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 12:36:40 +0530

    JG: update header add ant design menu button for toggle

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Space, Avatar } from "antd";
-import { MenuOutlined, UserOutlined } from "@ant-design/icons";
+import { MenuOutlined, CloseOutlined, UserOutlined } from "@ant-design/icons";
 import style from "./index.module.scss";
 
-function Navbar() {
+interface MenuTypes {
+  collapsed?: boolean;
+  toggleCollapsed?: () => void;
+}
+function Navbar({ collapsed, toggleCollapsed }: MenuTypes) {
   return (
     <header className={style.navbar}>
       <Space>
-        <div className={style.MenuIcon}>
-          <MenuOutlined />
+        <div className={style.MenuIcon} onClick={toggleCollapsed}>
+          {collapsed ? <MenuOutlined /> : <CloseOutlined />}
         </div>
         <div className={style.logo}>TakeNote</div>
       </Space>
