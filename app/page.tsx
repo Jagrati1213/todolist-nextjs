e575554b8c48f12c2b010f4e8b1c7fd00c12afb1
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 18:57:19 +0530

    Jg: create login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import { Button, Row } from "antd";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 function HomePage() {
-  return <div>Login Page</div>;
+  const buttonStyle = {
+    background: "#60463b",
+    color: "#feefc3",
+    height: "3rem",
+    padding: "0 3rem",
+    border: "none",
+  };
+
+  const router = useRouter();
+  const user = "ram";
+
+  useEffect(() => {
+    if (user !== null) {
+      return router.push("/dashboard");
+    } else {
+      return router.push("/");
+    }
+  }, [user]);
+
+  return (
+    <Row justify={"center"} align={"middle"} style={{ height: "100vh" }}>
+      <Button style={buttonStyle}>Sign In with Google</Button>
+    </Row>
+  );
 }
 
 export default HomePage;
