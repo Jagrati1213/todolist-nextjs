bd77318103bb875b175c31a321727311189b0751
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-10 18:37:29 +0530

    JG: update path

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { Col } from "antd";
 import React, { useState } from "react";
-import Navbar from "../Navbar";
-import SideMenu from "../SideMenu";
+import Navbar from "./Navbar";
+import SideMenu from "./SideMenu";
 
 export function Dashboard() {
   const [collapsed, setCollapsed] = useState(false);
